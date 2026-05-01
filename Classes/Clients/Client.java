6e608ae3cc8efe2d9ed40ec8fd10bc9e0ce3336a
Gianluca-Vanier <gzambito28@gmail.com>
2026-05-01 14:23:15 -0400

    Added code to Client.

--- Classes/Clients/Client.java
+++ Classes/Clients/Client.java
@@ -2,6 +2,10 @@ package Classes.Clients;
 
 import java.util.ArrayList;
 import Classes.Accounts.Account;
+import Classes.Accounts.ChequeingAccount;
+import Classes.Accounts.InvestmentAccount;
+import Classes.Accounts.SavingsAccount;
+import Exceptions.MissingChequeingAccountException;
 
 public abstract class Client 
 {
@@ -17,13 +21,34 @@ public abstract class Client
         this.password = password;
     }
 
-    /* void addAccount(Account acc){
-        IF acc is Savings OR Investment
-            CHECK if client has ChequeingAccount
-            IF NOT
-                THROW MissingChequeingAccountException
-        ADD acc to accounts list  
-    } */
+    void addAccount(Account acc){
+        if(acc instanceof SavingsAccount || acc instanceof InvestmentAccount){
+            boolean hasChequeing = false;
+
+            for(Account a : accounts){
+                if(a instanceof ChequeingAccount){
+                    hasChequeing = true;
+                    break;
+                }
+            }
+            
+            try{
+                if(!hasChequeing){
+                    throw new MissingChequeingAccountException();
+                }  
+            } 
+            catch(MissingChequeingAccountException e){
+                System.out.println(e);
+            }
+        }
+        
+        accounts.add(acc); 
+    }
+
+    public void transfer(Account from, Account to, double amount){
+        from.withdraw(amount);
+        to.deposit(amount);
+    }
     
     public boolean login(String id, String password){
         if(clientID.equals(id) && this.password.equals(password)){
