2c29be2c1bcd99dd8e6cfeaded01337c13a5d7b4
Gianluca-Vanier <gzambito28@gmail.com>
2026-05-02 23:00:09 -0400

    Created dashboard scene

--- GUI/DashboardController.java
+++ GUI/DashboardController.java
@@ -1,13 +1,35 @@
 package GUI;
 
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import Classes.Clients.Client;
+import javafx.fxml.FXML;
 
 public class DashboardController
 {
     private Client currentClient;
 
+    @FXML
+    private Label clientID;
+    @FXML
+    private Label username;
+    @FXML
+    private Label date;
+    @FXML
+    private Button transaction;
+    @FXML
+    private Button addAccount;
+
     public void setClient(Client client) {
         this.currentClient = client;
-        loadClientData();
+
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        date.setText(today.format(formatter));
+
+        //loadClientData();
     }
 }
